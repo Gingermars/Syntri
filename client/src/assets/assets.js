38164021f5cfc0996caaf498587ec95501fd8dba
Gingermars <umarkamorudeen10@gmail.com>
2026-03-29 19:57:48 +0100

    fix case sensitivity

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from "./Logo.png";
+import logo from "./logo.png";
 import gradientBackground from "./gradientBackground.png";
 import user_group from "./user_group.png";
 import star_icon from "./star.png";
